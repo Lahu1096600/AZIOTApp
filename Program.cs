7a85e0ea804a9d2513b6e22ea8b6b4270bbbe409
Lahu Kumbhar <Lahu_1664446327366@mindtreeosmosis.onmicrosoft.com>
2022-10-28 04:41:06 +0000

    addded json formatter

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 using AZIotApp.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+                {
+
+                    // Use the default property (Pascal) casing
+
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+                });
 
 builder.Services.AddTransient<IIotDeviceCRUD, IOTDeviceCRUDRepository>();
 builder.Services.AddTransient<IIOTDeviceProperty, IOTDevicePropertyRepository>();
@@ -17,10 +28,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
